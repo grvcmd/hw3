c719697c5d0055706567dd22562c9f0cb7d92910
Ken Valverde <verify>
2020-06-21 17:24:45 -0500

    Defined output_roster() function.

--- soccer_team_roster.py
+++ soccer_team_roster.py
@@ -2,14 +2,33 @@
 """This program will store roster and rating information for a soccer team.
     Coaches rate players during tryouts to ensure a balanced team."""
 
-# jersey number: key
-# player rating: value
+# key: jersey number (0 - 99)
+# value: player rating (1 - 9)
 player_dict = {}
-sorted_list_keys = [] # sorted list to store the jersey numbers
+sorted_list_keys = []  # sorted list to store the jersey numbers
+
 
 # function to sort dictionary keys
-def sortDictKeys():
+def sort_dict_keys():
     # sort the dictionary keys and then store them into
     # a list, then return that list
     sorted_list_keys = sorted(player_dict.keys())
     return sorted_list_keys
+
+
+# Define the function output_roster().
+def output_roster():
+
+    # Call the sort_dict_keys() function to get
+    # sorted list of dictionary keys.
+    sorted_list_keys = sort_dict_keys()
+    print('ROSTER')
+
+    # For loop to run through the list of keys
+    for i in sorted_list_keys:
+
+        # Display jersey number and player's rating
+        # sorted by jersey number.
+        print("Jersey number: {}, Rating: {}".format(i, player_dict[i]))
+
+
