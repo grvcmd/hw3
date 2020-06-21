d26b892d6fb175900ea6bbd65bf8252b0005d6cb
Ken Valverde <verify>
2020-06-21 17:41:10 -0500

    Added add_player() function.

--- soccer_team_roster.py
+++ soccer_team_roster.py
@@ -16,9 +16,9 @@ def sort_dict_keys():
     return sorted_list_keys
 
 
+# OBJ 3:
 # Define the function output_roster().
 def output_roster():
-
     # Call the sort_dict_keys() function to get
     # sorted list of dictionary keys.
     sorted_list_keys = sort_dict_keys()
@@ -26,9 +26,38 @@ def output_roster():
 
     # For loop to run through the list of keys
     for i in sorted_list_keys:
-
         # Display jersey number and player's rating
         # sorted by jersey number.
         print("Jersey number: {}, Rating: {}".format(i, player_dict[i]))
+
+
+# OBJ 4:
+# Define add_player() function.
+def add_user():
+    # Prompt user to enter a jersey number
+    print("Enter a new player's jersey number:")
+    jersey_num = int(input())
+
+    # Check if jersey number is between 0 - 99.
+    # If not, ask user to enter a jersey number again.
+    while ((jersey_num < 0) or (jersey_num > 99)):
+        print("Invalid Jersey Number! Please enter again!")
+        print("Enter a new player's jersey number:")
+        jersey_num = int(input())
+
+    # Prompt user to enter player's rating.
+    print("Enter the player's rating:")
+    player_rating = int(input())
+
+    # Check if player_rating is between 1 - 9
+    # If not, ask user to enter player_rating again.
+    while ((player_rating < 1) or (player_rating > 9)):
+        print("Invalid Rating! Please enter again!")
+        print("Enter the player's rating:")
+        player_rating = int(input())
+
+    # Update the player dictionary (player_dict) with
+    # the new entry.
+    player_dict.update({jersey_num : player_rating})
 
 
